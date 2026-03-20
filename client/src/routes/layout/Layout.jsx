5cb3901ce2848505d3e4f546de3591b52bd6e440
alphaMale28 <manandharsanil007@gmail.com>
2026-03-20 19:47:48 -0400

    fix: case-sensitivity issue

--- client/src/routes/layout/Layout.jsx
+++ client/src/routes/layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 
 import Navbar from "../../components/navbar/Navbar";
-import "./layout.scss";
+import "./Layout.scss";
 
 function Layout() {
   return (
